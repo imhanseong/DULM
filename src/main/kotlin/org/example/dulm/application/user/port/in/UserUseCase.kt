75127edd5cffe2c1b9232e08fcc0c77cc5cd8027
imhanseong <hans.hanseong@gmail.com>
2025-12-02 11:17:41 +0900

    fix :: UserUseCase 수정

--- src/main/kotlin/org/example/dulm/application/user/port/in/UserUseCase.kt
+++ src/main/kotlin/org/example/dulm/application/user/port/in/UserUseCase.kt
@@ -1,8 +1,9 @@
 package org.example.dulm.application.user.port.`in`
 
+import org.example.dulm.presentation.dto.response.UserResponse
 import org.springframework.http.ResponseEntity
 
 interface UserUseCase {
-    fun signUp(email : String, password : String, nickname : String) : UserDto
-    fun getUser(id : Long): UserDto
+    fun signUp(email : String, password : String, nickname : String) : UserResponse
+    fun getUser(id : Long) : UserResponse
 }
